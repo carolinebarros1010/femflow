65e4c5619f9f75762100224e33f3117e68526377
ricardojr <carolinebarros1010@gmail.com>
2026-02-04 18:18:31 -0300

    Remove env-staging from PROD app folder

--- app/js/env-staging.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  const STAGING_EXEC =
-    "https://femflowapi.falling-wildflower-a8c0.workers.dev/staging";
-
-  window.FEMFLOW = window.FEMFLOW || {};
-  window.FEMFLOW.SCRIPT_URL = STAGING_EXEC;
-  window.FEMFLOW.API_URL = STAGING_EXEC;
-  window.SCRIPT_URL = STAGING_EXEC;
-})();
