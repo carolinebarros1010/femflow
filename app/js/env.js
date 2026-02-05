93b82282a8aaff66d04b7aac98ca9dfe61e2b53b
ricardojr <carolinebarros1010@gmail.com>
2026-02-05 17:19:42 -0300

    Update env.js

--- app/js/env.js
+++ app/js/env.js
@@ -1,9 +1,10 @@
 (function () {
   const PROD_EXEC =
-    "https://femflowapi.falling-wildflower-a8c0.workers.dev/";
+    "https://femflowapi.falling-wildflower-a8c0.workers.dev";
 
   window.FEMFLOW = window.FEMFLOW || {};
-  window.FEMFLOW.SCRIPT_URL = STAGING_EXEC;
-  window.FEMFLOW.API_URL = STAGING_EXEC;
-  window.SCRIPT_URL = STAGING_EXEC;
+  window.FEMFLOW.ENV = "prod";
+  window.FEMFLOW.SCRIPT_URL = PROD_EXEC;
+  window.FEMFLOW.API_URL = PROD_EXEC;
+  window.SCRIPT_URL = PROD_EXEC;
 })();
