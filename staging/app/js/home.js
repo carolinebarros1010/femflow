7c729a4d137f24ac7a688550c935ab34fcc9be6b
ricardojr <carolinebarros1010@gmail.com>
2026-02-13 08:08:41 -0300

    Remove staged card image binaries; keep code-only thumbnail mapping

--- staging/app/js/home.js
+++ staging/app/js/home.js
@@ -666,12 +666,12 @@ const CARD_THUMBS = {
   personal: "personal_ricardojr.jpg",
   personal_ricardojr: "personal_ricardojr.jpg",
   corrida_curta: "corrida_curta.jpg",
-  planilha_corrida_5k: "corrida_curta.jpg",
-  planilha_corrida_10k: "corrida_Longa.jpg",
-  planilha_corrida_15k: "corrida_Longa.jpg",
-  corrida_5k: "corrida_curta.jpg",
-  corrida_10k: "corrida_Longa.jpg",
-  corrida_15k: "corrida_Longa.jpg"
+  planilha_corrida_5k: "corrida_5k.jpg",
+  planilha_corrida_10k: "corrida_10k.jpg",
+  planilha_corrida_15k: "corrida_15k.jpg",
+  corrida_5k: "corrida_5k.jpg",
+  corrida_10k: "corrida_10k.jpg",
+  corrida_15k: "corrida_15k.jpg"
 };
 
 function getThumbUrl(enfase) {
